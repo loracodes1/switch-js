ca381950256507e71ca8ada9019fba61d0bbdecf
florence <loracodes3@gmail.com>
2024-09-30 22:07:45 +0300

    case and break

--- index.js
+++ index.js
@@ -18,3 +18,39 @@ switch (order) {
 }
 console.log(order)
 console.log(ingredients)
+
+
+//ALARM SWITCH
+//Monday 7am
+//Tuesday-thursday 4am
+//friday 10am
+//sat 9am
+//sunday 9am
+
+let day ='Monday';
+switch(day){
+    case:'Monday'
+    console.log("7am")
+    break
+    case:'Tuesday'
+    console.log()
+    break
+    case:'Wednesday'
+    console.log()
+    break
+    case:'Thursday'
+    console.log()
+    break
+    case:'Friday'
+    console.log()
+    break
+    case:'Saturday'
+    console.log()
+    break
+    case:'Sunday'
+    console.log()
+    break
+    default:''
+}
+
+
