163e2c6186b46b3d059978d031600d70bca5c780
florence <loracodes3@gmail.com>
2024-09-30 19:34:44 +0300

    output ingredients

---
+++ index.js
@@ -0,0 +1,20 @@
+const order = 'cheeseburger';
+
+let ingredients;
+
+switch (order) {
+    case 'cheeseburger':
+        ingredients = 'bun, burger, cheese, lettuce, tomato, onion';
+        break;
+    case 'hamburger':
+        ingredients = 'bun, burger, lettuce, tomato, onion';
+        break;
+    case 'malted':
+        ingredients = 'milk, ice cream, malted milk powder';
+        break;
+    default:
+        console.log("Sorry, that's not on the menu right now.");
+        break;
+}
+console.log(order)
+console.log
